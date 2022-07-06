ca28ce0fa116ca464ef8849cf5047b152ac2ae05
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-07-06 21:24:01 +0530

    Update checkTemperature.cpp

--- checkTemperature.cpp
+++ checkTemperature.cpp
@@ -4,9 +4,9 @@ using namespace std;
 bool checkTemp(float temperature)
 {
    bool Result = true;
-   if(temperature < 0 || temperature > 45) {
-      cout << "Temperature out of range!\n";
-      Result = false;
+   if(temperature < 0 || temperature > 45){
+     cout << "Temperature out of range!\n";
+     Result = false;
    }
    return Result;
 }
